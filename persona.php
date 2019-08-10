3efb1f582f25183fe121228a16fd95827cb368bb
Gama56 <cristhianesquivel@hotmail.es>
2019-08-09 21:06:28 -0600

    Proyecto

---
+++ persona.php
@@ -0,0 +1,9 @@
+<?php
+echo $nombre = $_POST['Nombre'],"<br>";
+echo $apellido = $_POST['Apellido'],"<br>";
+echo $telefono = $_POST['Telefono'],"<br>";
+echo $email = $_POST['Email'],"<br>";
+echo $codigodelcurso = $_POST['CodigodelCurso'],"<br>";
+echo $nombredelcurso = $_POST['NombredelCurso'],"<br>";
+echo $horarior = $_POST['horario'],"<br>";
+?>
